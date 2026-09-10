2e0ed145a5c9feec13fdcc46f6a07b5be50206ae
Kasperihelin1 <kasperi.helin@student.laurea.fi>
2026-09-10 14:45:51 +0300

    nytonviimene

--- WS02_javascript/js/Script.js
+++ WS02_javascript/js/Script.js
@@ -30,15 +30,14 @@ console.log("Terve " + visitorName + "! Tervetuloa javacriptin pariin!");
 const visitorFavoriteAnimal = prompt("Mikä on sinun suosikkieläimesi?");
 
 // Create a sentence using both answers
-console.log("Terve " + visitorName + "! Sinun lempi oliosi on " + visitorFavoriteAnimal + ".");
+console.log("Terve " + visitorName + "! Sinun lempi eläimesi on " + visitorFavoriteAnimal + ".");
 // console.log("Hello, " + visitorName + "! Your favorite animal is " + visitorFavoriteAnimal + ".");
 
 // ⭐⭐ BONUS Ask the user for their favorite animal.If their favorite animal is "Blue Elephant", print: Great choice! That's my favorite animal too!
 if (visitorFavoriteAnimal === favoriteAnimal || visitorFavoriteAnimal === "koira") {
-    console.log("Great choice! That's my favorite animal too!");
+    console.log("Loistava valinta! Se on myös minun lempieläimeni!");
 } else {
-    console.log("Nice! Your favorite animal is " + visitorFavoriteAnimal + ".");
+    console.log("Hyvä valinta! sinun lempi eläimesi on " + visitorFavoriteAnimal + ".");
 }
 // ⭐⭐ BONUS Otherwise, print: Nice! Your favorite animal is [animal]. Can you figure out how to do this using if / else?
 
-// const visitorFavoriteAnimal = prompt("What is your favorite animal?");
